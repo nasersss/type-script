7db0cc633ace96cde8199175f760cae79770ba38
Nasser Al-Ghaithi <nasser.al-ghaithi@jisr.net>
2023-05-14 14:11:09 +0300

    void never

---
+++ 7-void_never/index.js
@@ -0,0 +1,23 @@
+/**
+ * Data types
+ * --void
+ * ----Function that will be return nothing.
+ * ---- function in js that that not  return a value is show undefined
+ * ---- undefined is not void
+ *
+ * -- Never
+ * ---- Return type never returns.
+ * ---- The function does not have a normal completion
+ * ---- it throws an error or never finish running at all "infinite loop"
+ *  */
+function login(msg) {
+    console.log('Ali');
+}
+console.log(login('Hi every one '));
+function alwaysLog(name) {
+    while (true) {
+        console.log(name);
+    }
+}
+alwaysLog('sdlfjgh');
+console.log(login('Hi every one '));
