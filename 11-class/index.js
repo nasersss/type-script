db6b967be2b53fcc0b2b23b636d677fe067bc8eb
Nasser Al-Ghaithi <nasser.al-ghaithi@jisr.net>
2023-05-14 20:53:20 +0300

    class

--- 11-class/index.js
+++ 11-class/index.js
@@ -1,17 +1,54 @@
+/**
+ * --Class
+ * ----Data Access modifiers && parameters properties
+ * ------Public
+ * -------- All members of a class in typescript are public
+ * -------- All public member can be accessed anywhere without any restriction
+ * ------Private
+ * -------- Members are visible only to that class and are not accessible outside the class
+ * ------Protected
+ * -------- Same like private but can be access using the deriving class
+ * --TypeScript is a layer on top of javascript
+ * ------ It should remove all annotation and although access modifiers "Private for example"
+
+ *
+*/
 var User = /** @class */ (function () {
-    function User(username, salary) {
-        this.u = username;
-        this.s = salary;
+    // constructor(username: string,salary:number) {
+    //     this.u = username;
+    //     this.s = salary;
+    //     this.msg = function() {
+    //         return `Hello ${this.u} your salary ${this.s}`
+    //     }
+    // }
+    function User(_u, s, address) {
+        this._u = _u;
+        this.s = s;
+        this.address = address;
         this.msg = function () {
-            return "Hello ".concat(this.u, " your salary ").concat(this.s);
+            return "Hello ".concat(this._u, " your salary ").concat(this.s, " address ").concat(this.address);
         };
     }
+    Object.defineProperty(User.prototype, "u", {
+        get: function () {
+            return this._u;
+        },
+        set: function (value) {
+            this._u = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
     User.prototype.sayMsg = function () {
-        return "Hello ".concat(this.u, " your salary ").concat(this.s);
+        return "Hello ".concat(this._u, " your salary ").concat(this.s, " address ").concat(this.address);
     };
     return User;
 }());
-var userOne = new User('Ali', 2500);
-console.log(userOne.u);
-console.log(userOne.s);
+var userOne = new User('Ali', 2500, 'Yemen');
+var userSecond = new User('Nasser', 3000);
 console.log(userOne.msg());
+// console.log(userOne.u);//Property 'u' is private and only accessible within class 'User'
+// console.log(userOne.s);//Property 's' is protected and only accessible within class 'User' and its subclasses
+userOne.u = "Ahmed";
+console.log(userOne.msg());
+console.log(userSecond.msg());
--- 11-class/index.ts
+++ 11-class/index.ts
@@ -1,22 +1,50 @@
+/**
+ * --Class
+ * ----Data Access modifiers && parameters properties
+ * ------Public
+ * -------- All members of a class in typescript are public
+ * -------- All public member can be accessed anywhere without any restriction
+ * ------Private
+ * -------- Members are visible only to that class and are not accessible outside the class
+ * ------Protected
+ * -------- Same like private but can be access using the deriving class
+ * --TypeScript is a layer on top of javascript
+ * ------ It should remove all annotation and although access modifiers "Private for example"
+
+ * 
+*/
 class User {
-    u:string;
-    s:number;
-    msg: () => string;
-
-    constructor(username: string,salary:number) {
-        this.u = username;
-        this.s = salary;
-        this.msg = function() {
-            return `Hello ${this.u} your salary ${this.s}`
-        }
+    public get u(): string {
+        return this._u;
     }
-
+    public set u(value: string) {
+        this._u = value;
+    }
+//    private u:string;
+//    protected s:number;
+    msg: () => string;
+    // constructor(username: string,salary:number) {
+    //     this.u = username;
+    //     this.s = salary;
+    //     this.msg = function() {
+    //         return `Hello ${this.u} your salary ${this.s}`
+    //     }
+    // }
+    constructor (private _u: string, protected s:number, public address?:string){
+        this.msg = function() {
+                    return `Hello ${this._u} your salary ${this.s} address ${this.address}`
+         }
+    }
     sayMsg(){
-        return `Hello ${this.u} your salary ${this.s}`
+        return `Hello ${this._u} your salary ${this.s} address ${this.address}`
     }
 }
 
-let userOne = new User('Ali',2500);
-console.log(userOne.u);
-console.log(userOne.s);
+let userOne = new User('Ali',2500,'Yemen');
+let userSecond= new User('Nasser',3000);
 console.log(userOne.msg());
+// console.log(userOne.u);//Property 'u' is private and only accessible within class 'User'
+// console.log(userOne.s);//Property 's' is protected and only accessible within class 'User' and its subclasses
+userOne.u = "Ahmed";
+console.log(userOne.msg());
+console.log(userSecond.msg());
